25e163732c94d353c09c7891622342168e76a729
areida <andrew@syn0.com>
2011-02-25 13:08:48 -0700

    Checking for existance of crons dir

--- classes/minion/task/app/cron/generate.php
+++ classes/minion/task/app/cron/generate.php
@@ -7,6 +7,11 @@ class Minion_Task_App_Cron_Generate extends Minion_Task {
 		$view = Kostache::factory('minion/task/app/cron/generate');
 		$timestamp = $view->timestamp();
 
+		if ( ! is_dir(APPPATH.'crons'))
+		{
+			mkdir(APPPATH.'crons');
+		}
+		
 		file_put_contents(APPPATH.'crons'.DIRECTORY_SEPARATOR.$timestamp.'.php', $view->render());
 	}
 }
